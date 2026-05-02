bc4214eecca80a7ba29ed0bbaabc407b016b835e
NeverKnower <neverknowerdev@gmail.com>
2026-05-02 13:11:03 +0200

    fixed and improvements

--- main.go
+++ main.go
@@ -75,7 +75,10 @@ func main() {
 	r.Use(middleware.Recoverer)
 
 	r.Route("/api", func(r chi.Router) {
-		r.Use(srv.E2EMockMiddleware)
+		if os.Getenv("E2E_MODE") == "true" {
+			log.Println("E2E mode enabled - mock middleware active")
+			r.Use(srv.E2EMockMiddleware)
+		}
 		r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("pong"))
 		})
